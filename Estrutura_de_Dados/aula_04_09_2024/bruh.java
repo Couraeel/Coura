aa6f907087387dbc5ce006f88500e7a5ed21be59
Couraeel <rafael.coura@sempreceub.com>
2024-09-04 15:16:12 -0300

    Add files via upload

---
+++ Estrutura_de_Dados/aula_04_09_2024/bruh.java
@@ -0,0 +1,20 @@
+package aula_04_09_2024;
+
+import java.util.HashMap;
+
+public class bruh {
+	
+	public static void main(String[] args) {
+		
+		HashMap<String, String> things = new HashMap<>();
+		things.put("Rafael", "lindio");
+		things.put("MatHeus", "gaymer");
+		things.put("Richard", "trouxa");
+		
+		//things.remove("");
+		//things.clear();
+		//things.size();
+		/*System.out.println(things.get("Rafael"));*/
+		System.out.println(things);
+	}	
+}
