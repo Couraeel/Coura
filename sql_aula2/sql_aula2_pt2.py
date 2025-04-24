f7b50214d4f76d40c5adc1b671b54e4696af6d87
Couraeel <rafael.coura@sempreceub.com>
2025-04-24 10:24:58 -0300

    Update sql_aula2_pt2.py

--- sql_aula2/sql_aula2_pt2.py
+++ sql_aula2/sql_aula2_pt2.py
@@ -3,19 +3,26 @@ import mysql.connector
 conexao = mysql.connector.connect(user = 'root',
                                      password = 'ceub123456',
                                      host = '127.0.0.1',
-                                     database = 'db_empresa')
+                                     database = 'db_loja')
 
 print('conexao',conexao)
 cursor = conexao.cursor()
-sql = '''
-CREATE TABLE IF NOT EXISTS tb_funcionario (
-    idt INTEGER,
-    name VARCHAR(46) NOT NULL,
-    salario DECIMAL(9,2) NULL,
-    PRIMARY KEY (idt)
-)
+
+search_selc = input("nome: ")
+
+sql = '''SELECT CODIGO 
+FROM produto
+where nome = '{search_selec}'
 '''
+
+#sql = f'''delete 
+#FROM produto
+#where nome = '{search_selec}'
+#'''
+
+
 cursor.execute(sql)
+print(cursor.fetchall())
 cursor.close()
 conexao.close()
 print("Conexão com o banco de dados fechada.")
