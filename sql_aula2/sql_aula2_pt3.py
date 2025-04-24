89c7c01ec933a60f04f7594aa1418faecfeb265a
Couraeel <rafael.coura@sempreceub.com>
2025-04-24 10:13:10 -0300

    Update sql_aula2_pt3.py

--- sql_aula2/sql_aula2_pt3.py
+++ sql_aula2/sql_aula2_pt3.py
@@ -1,16 +1,29 @@
-import sqlite3
+import mysql.connector
 
-conexao = sqlite3.connect('db_empresa.db')
+conexao = mysql.connector.connect(user='root',
+                                   password='ceub123456',
+                                   host='127.0.0.1',
+                                   database='db_loja')
+print('Conexão estabelecida:', conexao)
 cursor = conexao.cursor()
 
-sql = '''insert into tb_funcionario
-    (idt, name, salario)
-    values('451', 'Lorena', 4500.00)'''
 
+prod_name = input("Digite o nome do produto: ")
+prod_preco = float(input("Digite o preço do produto: "))
+prod_date = input("Digite a data de validade do produto (YYYY-MM-DD): ")
 
-cursor.execute(sql)
-conexao.commit()
-cursor.close()
-conexao.close()
+sql = '''INSERT INTO tb_produtos (name, preco, data_validade) VALUES (%s, %s, %s)'''
+values = (prod_name, prod_preco, prod_date)
 
-print("Fechou a base de dados")
+try:
+    cursor.execute(sql, values)
+    conexao.commit()  # Commit the transaction
+
+    print('Registros inseridos:', cursor.rowcount)
+    print('cursor.statement', cursor.statement)
+except mysql.connector.Error as err:
+    print("Erro ao inserir dados:", err)
+finally:
+    cursor.close()
+    conexao.close()
+    print("Conexão com o banco de dados fechada.")
