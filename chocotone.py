8e4fbea11609b7c679b8eba703546bc87d8970ff
Couraeel <rafael.coura@sempreceub.com>
2025-08-18 10:52:43 -0300

    Add files via upload

---
+++ chocotone.py
@@ -0,0 +1,27 @@
+#ricardo                                #matue
+corrida (vrum vrum)                     platinar jogo
+teamfortress2                           sabe ler
+ciclismo                                nerd (conhece tudo) [ele sabe dms]
+cozinhar                                beijar rapazes
+
+#lorena                                 #ragael
+valorant                                dinossauro
+volei                                   musica (nn conhece musica)
+beijar muie                             speedrunners
+design                                  pokemon
+
+#guiljeme                               #luiz
+anime                                   farmar aura
+cosplay                                 jogar
+academia                                reels
+capoeira                                conhecimento em metal (MÚSICA)
+
+#luan                                   #içami
+ser emo                                 IzAMI
+conhecimento em animais
+outlast
+filmes de terror
+
+
+
+IçAMI acima de tudo IzAMI só em cima de mim 
